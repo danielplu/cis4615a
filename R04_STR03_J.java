c189279b57af8c89491cff21fa9b122703d5e96d
danielplu <ludaniel@knights.ucf.edu>
2020-10-25 00:48:56 -0400

    Completed R04_STR03_J

---
+++ R04_STR03_J.java
@@ -0,0 +1,25 @@
+package cis4615;
+
+import java.math.BigInteger;
+
+/*
+ * Daniel Lu
+ * Rule 04. Do not encode noncharacter data as a string, R04_STR03-J.java
+ * Non-compliant code
+ * 		BigInteger x = new BigInteger("530500452766");
+		byte[] byteArray = x.toByteArray();
+		String s = new String(byteArray);
+		byteArray = s.getBytes();
+		x = new BigInteger(byteArray);
+ */
+
+public class R04_STR03_J {
+	// Compliant solution
+	public static void main(String[] args) {
+		BigInteger x = new BigInteger("530500452766");
+		String s = x.toString();  // Valid character data
+		byte[] byteArray = s.getBytes();
+		String ns = new String(byteArray);
+		x = new BigInteger(ns);
+	}
+}
